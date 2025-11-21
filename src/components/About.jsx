a952fff82f659a0431ce6811239fdf9d7d42825c
abhisheksamriya <abhisheksamriya2004@gmail.com>
2025-11-21 17:00:53 +0530

    mail changed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -60,7 +60,7 @@ const About = () => {
                 RESUME.pdf
               </a>
               <a
-                href="mailto:abhisheksamriya2004@gmail.com"
+                href="mailto:abhisheksamriya2024@gmail.com"
                 className="inline-flex items-center justify-center gap-2 border border-green-400 text-green-400 px-4 py-2 hover:bg-green-400 hover:text-black transition-colors w-full sm:w-auto rounded-xl"
               >
                 <CiMail />
